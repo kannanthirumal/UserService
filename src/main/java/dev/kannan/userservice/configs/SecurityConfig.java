3e7416baa7824e7cfa5783d24d0e1f07b6c80ae4
kannanthirumal <kannantecht@gmail.com>
2025-09-11 14:31:18 +0530

    Add Spring Security OAuth2 config and SecurityFilterChain beans - Add OAuth2 config settings in application.properties - Define SecurityFilterChain bean and Spring OAuth2 Authorization   related beans in "SecurityOauthConfig"

--- src/main/java/dev/kannan/userservice/configs/SecurityConfig.java
+++ src/main/java/dev/kannan/userservice/configs/SecurityConfig.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
+  - Added this bean snippet inside the "securityFilterChain" method
+  - of the "SecurityOauthConfig" class
+  - to prevent creation of multiple conflicting "SecurityFilterChain" beans
+  - which caused the "Error creating bean with name 'springSecurityFilterChain'" exception
+  - by ensuring only a single SecurityFilterChain bean is created for Spring Security configuration
+*/
+
 @Configuration
 public class SecurityConfig {
     /*
@@ -14,16 +22,16 @@ public class SecurityConfig {
         - I'm just customizing the end points that doesn't need any auth
     */
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/signup").permitAll()
-                        .anyRequest().permitAll()  // Or use `.authenticated()` if required
-                )
-                .cors().disable()
-                .csrf().disable();
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/signup").permitAll()
+//                        .anyRequest().permitAll()  // Or use `.authenticated()` if required
+//                )
+//                .cors().disable()
+//                .csrf().disable();
+//
+//        return http.build();
+//    }
 }
