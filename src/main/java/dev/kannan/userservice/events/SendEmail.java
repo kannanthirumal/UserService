151005ef8141cf6f57ba2a03081df29c62eae694
kannanthirumal <kannantecht@gmail.com>
2025-09-25 12:25:55 +0530

    Create SendEmail DTO for Kafka event and organize under events folder

--- src/main/java/dev/kannan/userservice/events/SendEmail.java
+++ src/main/java/dev/kannan/userservice/events/SendEmail.java
@@ -1,4 +1,13 @@
 package dev.kannan.userservice.events;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SendEmail {
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
 }
